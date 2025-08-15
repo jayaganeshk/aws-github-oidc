a0aac35bc913669b02c08c239fb0f5761487b208
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2025-08-15 13:40:09 +0530

    add env to tf bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-locals {
-  bucket_name = lower(format("%s-%s-%s", var.bucket_name_prefix, var.environment, data.aws_caller_identity.current.account_id))
-
-  common_tags = merge({
-    Project     = "github-oidc-demo"
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }, var.tags)
-}
-
 resource "aws_s3_bucket" "demo" {
-  bucket = local.bucket_name
-  tags   = local.common_tags
+  bucket = "github-oidc-demo-${data.aws_caller_identity.current.account_id}-${var.environment}"
 }
